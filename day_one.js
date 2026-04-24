dab70da84eb16f50e3f0cf4c529a585682c2b05e
codebyaddro <codebyaddro@gmail.com>
2026-04-24 14:26:09 +0600

    day_one_commit

---
+++ day_one.js
@@ -0,0 +1,66 @@
+// sum of two numbers
+// let a = 10;
+// let b = 20;
+// let sum = a + b;
+// console.log(sum);
+
+// sum of number and string
+// let a = 10;
+// let b = "20";
+// let sum = a + b;
+// console.log(sum);
+
+// value swapping via 7 ways
+// let a = 10;
+// let b = 20;
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+// value swapping
+// let a = 10;
+// let b = 20;
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// value swapping
+// let a = 10;
+// let b = 20;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a, b);
+
+// value swapping
+// let a = 40;
+// let b = 20;
+// a = a * b;
+// b = a / b;
+// a = a / b;
+// console.log(a, b);
+
+// value swapping
+// let a = 10;
+// let b = 20;
+// a = a ^ b;
+// b = a ^ b;
+// a = a ^ b;
+// console.log(a, b);
+
+// value swapping
+// let a = 70;
+// let b = 40;
+// a = a - b;
+// b = a + b;
+// a = b - a;
+// console.log(a, b);
+
+// value swap through function return
+// let a = 10;
+// let b = 20;
+// function swap(a, b) {
+//   return [b, a];
+// }
+// [a, b] = swap(a, b);
+// console.log(a, b);
