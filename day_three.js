401b902d4d59f563009b926a39bb76f60fe9d21b
codebyaddro <codebyaddro@gmail.com>
2026-04-26 11:41:36 +0600

    learning

---
+++ day_three.js
@@ -0,0 +1,14 @@
+// let n = Number(prompt("Enter the number"));
+
+// let n = 10;
+
+// if(isNaN(n)) {
+//     console.log("Something went wrong");
+//     return;
+// } else {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     console.log(sum);
+// }
