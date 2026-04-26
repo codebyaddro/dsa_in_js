47291a9dbeab0d8c8fe36c31c69caa971a5a67ad
codebyaddro <codebyaddro@gmail.com>
2026-04-26 11:43:51 +0600

    learning

--- day_three.js
+++ day_three.js
@@ -12,3 +12,23 @@
 //     }
 //     console.log(sum);
 // }
+
+
+// let n = Number(prompt("Enter the number"));
+
+// let n = 10;
+
+// if(isNaN(n)) {
+//     console.log("Something went wrong");
+//     return;
+// } else {
+//     if( n > 0) {
+//         let factorial = 1;
+//         for (let i = 1; i <= n; i++) {
+//             factorial *= i;
+//         }
+//         console.log(factorial);
+//     } else {
+//         console.log("Please enter a positive number");
+//     }
+// }
