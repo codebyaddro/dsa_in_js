20e9b442b8cc9b15c2d87ed4c1e5793cd6d8f63f
codebyaddro <codebyaddro@gmail.com>
2026-04-29 01:07:23 +0600

    learning

--- day_three.js
+++ day_three.js
@@ -49,3 +49,27 @@
 //     }
 //     console.log(n);
 // }
+
+
+// let num = 1234;
+// let sum = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;
+//     sum += digit; 
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(sum);
+
+
+// let num = 1234;
+// let reversed = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;
+//     reversed = reversed * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(reversed);
