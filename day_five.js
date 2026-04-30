aabce7c063bc9850fb61e76f92e6a63f78aa1f3b
codebyaddro <codebyaddro@gmail.com>
2026-05-01 01:55:49 +0600

    learning

--- day_five.js
+++ day_five.js
@@ -1,3 +1,19 @@
-let arr = new Array(10);
+// let arr = new Array(3);
 
-console.log(arr);
+// arr[0] = 10;
+// arr[1] = 20;
+// arr[2] = 30;
+// arr[3] = 40;
+
+// console.log(arr.length);
+
+
+// let prompt = require("prompt-sync")();
+
+// let arr = new Array(5);
+
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = Number(prompt("Enter the number = "));
+// }
+
+// console.log(arr);
