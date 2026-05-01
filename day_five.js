d88c64261d3b0c78858caea13508f9f69de24ba2
codebyaddro <codebyaddro@gmail.com>
2026-05-01 15:36:10 +0600

    learning

--- day_five.js
+++ day_five.js
@@ -17,3 +17,86 @@
 // }
 
 // console.log(arr);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+
+// console.log(sum);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let max = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];
+//     }
+// }
+
+// console.log(max);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }
+
+// console.log(min);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let max = Math.max(arr[0], arr[1]);
+// let sMax = Math.min(arr[0], arr[1]);
+
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] > max && max != arr[i]) {
+//         sMax = max;
+//         max = arr[i];
+//     }
+// }
+
+// console.log(sMax);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let revArr = []
+
+// for (let i = 0; i < arr.length; i++) {
+//     revArr.unshift(arr[i]);
+// }
+
+// console.log(revArr);
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let revArr = new Array(arr.length)
+// let j = 0;
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     revArr[j] = arr[i];
+//     j++;
+// }
+
+// console.log(revArr);
+
+
+let arr = [10, 20, 30, 40, 50];
+
+
+// let arr = [10, 20, 30, 40, 50];
+// let revArr = []
+
+// for (let i = 0; i < arr.length; i++) {
+//     revArr[i] = arr[arr.length - 1 - i];
+// }
+
+// console.log(revArr);
