dd673088c4cd34135761cccf5553e8541958acdb
codebyaddro <codebyaddro@gmail.com>
2026-05-03 01:38:18 +0600

    learning

--- day_six.js
+++ day_six.js
@@ -62,3 +62,17 @@
 // }
 
 // console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+
+// function maxProfit(prices) {
+//     let maxProfit = 0;
+//     let min = prices[0];
+//     for(let i = 0; i < prices.length; i++) {
+//         if(prices[i] < min) min = prices[i];
+//         let profit = prices[i] - min;
+//         maxProfit = Math.max(maxProfit, profit);
+//     }
+//     return maxProfit;
+// }
+
+// console.log(maxProfit([7,1,5,3,6,4]));
