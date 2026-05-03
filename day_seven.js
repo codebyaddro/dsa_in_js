eff7e42a8815fa100f7af8270b5a1e3a1d31fead
codebyaddro <codebyaddro@gmail.com>
2026-05-03 22:17:51 +0600

    learning

---
+++ day_seven.js
@@ -0,0 +1,56 @@
+// loop of string each character
+// let str = "Hello";
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+
+// reverse loop of string each character
+// let str = "Hello";
+// for (let i = str.length - 1; i >= 0; i--) {
+//     console.log(str[i]);
+// }
+
+
+// reverse string
+// let str = "Hello";
+// let revStr = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     revStr += str[i];
+// }
+// console.log(revStr);
+
+
+// is pallindrome
+// v1
+// let str = "malayalam";
+// let revStr = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     revStr += str[i];
+// }
+// if (str == revStr) {
+//     console.log("Pallindrome");
+// } else {
+//     console.log("Not Pallindrome");
+// }
+
+// v2
+// let str = "madam";
+// let ispallindrome = true;
+// let i = 0, j = str.length - 1;
+
+// while(i < j) {
+//     if (str[i] != str[j]) {
+//         ispallindrome = false;
+//         break;
+//     }
+//     i++;
+//     j--;
+// }
+// if (ispallindrome) {
+//     console.log("Pallindrome");
+// } else {
+//     console.log("Not Pallindrome");
+// }
