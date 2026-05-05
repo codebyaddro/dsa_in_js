1c32b3c984a337dcb11e9563d0fb78c2cd511b19
codebyaddro <codebyaddro@gmail.com>
2026-05-06 01:18:40 +0600

    insertion sort

--- day_eight.js
+++ day_eight.js
@@ -16,17 +16,34 @@
 
 
 // selection sort
-function selectionSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) minIndex = j;
+// function selectionSort(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         let minIndex = i;
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[j] < arr[minIndex]) minIndex = j;
+//         }
+//         let temp = arr[i];
+//         arr[i] = arr[minIndex];
+//         arr[minIndex] = temp;
+//     }
+//     return arr;
+// }
+
+// console.log(selectionSort([5, 4, 3, 2, 1]));
+
+
+// insertion sort
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        let temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        arr[j + 1] = key;
     }
     return arr;
 }
 
-console.log(selectionSort([5, 4, 3, 2, 1]));
+console.log(insertionSort([5, 4, 3, 2, 1]));
