d1062905de4a2055590ed2aa20650b6e5bcde61d
codebyaddro <codebyaddro@gmail.com>
2026-05-05 23:35:08 +0600

    learn bubble sort

--- day_eight.js
+++ day_eight.js
@@ -1,6 +1,6 @@
 // bubble sort
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
