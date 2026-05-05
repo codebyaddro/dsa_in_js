0918f5c9d15a40349dbcf83d35d6a01e6fa70598
codebyaddro <codebyaddro@gmail.com>
2026-05-06 00:21:16 +0600

    learn selection sort

--- day_eight.js
+++ day_eight.js
@@ -1,15 +1,32 @@
 // bubble sort
-function bubbleSort(arr) {
+// function bubbleSort(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         for (let j = 0; j < arr.length - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(bubbleSort([5, 4, 3, 2, 1]));
+
+
+// selection sort
+function selectionSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) minIndex = j;
         }
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
     return arr;
 }
 
-console.log(bubbleSort([5, 4, 3, 2, 1]));
+console.log(selectionSort([5, 4, 3, 2, 1]));
