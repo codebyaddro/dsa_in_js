442b03cf87643fb98f925b6ce1e4980fc0fc0fe2
codebyaddro <codebyaddro@gmail.com>
2026-05-06 01:47:52 +0600

    recursion started

--- day_eight.js
+++ day_eight.js
@@ -33,17 +33,17 @@
 
 
 // insertion sort
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let key = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-    return arr;
-}
+// function insertionSort(arr) {
+//     for (let i = 1; i < arr.length; i++) {
+//         let key = arr[i];
+//         let j = i - 1;
+//         while (j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
+//         arr[j + 1] = key;
+//     }
+//     return arr;
+// }
 
-console.log(insertionSort([5, 4, 3, 2, 1]));
+// console.log(insertionSort([5, 4, 3, 2, 1]));
