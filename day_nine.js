5cbd6bf1430bf8c2d5ea8c75e34b16f3d2213c82
codebyaddro <codebyaddro@gmail.com>
2026-05-09 22:35:26 +0600

    recursion 1 to n log

--- day_nine.js
+++ day_nine.js
@@ -1,7 +1,17 @@
+// n to 1 log
+// function temp(n) {
+//     if(n === 0) return
+//     console.log(n)
+//     temp(n-1)
+// };
+
+// temp(5);
+
+// 1 to n
 function temp(n) {
     if(n === 0) return
-    console.log(n)
     temp(n-1)
+    console.log(n)
 };
 
 temp(5);
