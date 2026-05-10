7b7b4413636dc0709b48ae08da686bee1bcf3893
codebyaddro <codebyaddro@gmail.com>
2026-05-10 17:48:51 +0600

    fabonacci with recursion

--- day_nine.js
+++ day_nine.js
@@ -22,8 +22,28 @@
 // console.log(sum(5));
 
 // factorial of n
-function fact(n) {
-    if (n == 1) return n
-    return n * fact(n - 1)
+// function fact(n) {
+//     if (n == 1) return n
+//     return n * fact(n - 1)
+// }
+// console.log(fact(5));
+
+// fibonacci of n
+// let first = 0;
+// let second = 1;
+// function fabonacci(n) {
+//     if (n == 0) return;
+//     console.log(first);
+//     let temp = first;
+//     first = second;
+//     second = temp + second;
+//     fabonacci(n - 1);
+// }
+// fabonacci(10);
+
+// fibonacci of n'th term
+function fabo(n) {
+    if(n==0 || n==1) return n;
+    return fabo(n-1) + fabo(n-2);
 }
-console.log(fact(5));
+console.log(fabo(6));
