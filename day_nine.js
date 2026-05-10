cc4513ff28c42d2d9cf3dc47bfe921ff40cfbeb4
codebyaddro <codebyaddro@gmail.com>
2026-05-10 16:03:56 +0600

    sum and factorial with recursion

--- day_nine.js
+++ day_nine.js
@@ -4,14 +4,26 @@
 //     console.log(n)
 //     temp(n-1)
 // };
-
 // temp(5);
 
-// 1 to n
-function temp(n) {
-    if(n === 0) return
-    temp(n-1)
-    console.log(n)
-};
+// 1 to n log
+// function temp(n) {
+//     if(n === 0) return
+//     temp(n-1)
+//     console.log(n)
+// };
+// temp(5);
 
-temp(5);
+// sum of n
+// function sum(n) {
+//     if (n == 1) return n
+//     return n + sum(n - 1)
+// }
+// console.log(sum(5));
+
+// factorial of n
+function fact(n) {
+    if (n == 1) return n
+    return n * fact(n - 1)
+}
+console.log(fact(5));
